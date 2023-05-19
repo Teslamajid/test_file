b99eb9bde27cf54e5af8e4b9dc2dfa55732a05ef
unknown <bahabdulmajid775@gmail.com>
2023-05-19 03:20:33 -1200

    this is the first commit

---
+++ majid.dart
@@ -0,0 +1,24 @@
+void main(){
+
+print("hello");
+String name = 'Tesla';
+print(name);
+var num1 = 25;
+var num2 = 50;
+var sum = num1 + num2;
+print(sum);
+
+int a = 75;
+int b = 50;
+int c = 75-50;
+print(c);
+
+if (c>25)
+{
+    print('it is great');
+}
+else if (c<25)
+{
+    print('n');
+}
+}
